abc80ad79e8b17cc301ea4b88e003fbb5446c41c
sardamed <mohamedsarda2001@gmail.com>
2024-08-26 19:37:36 +0100

    00/ex00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+
+int main(int c, char **ar)
+{
+    int i = 1;
+    unsigned long j = 0;
+
+
+    if (c < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (1);
+    }
+    std::string tmp;
+    while (i < c)
+    {
+        j = 0;
+        tmp = ar[i];
+        while (j < tmp.length())
+        {
+            if (tmp[j] >= 'a' && tmp[j] <= 'z')
+                tmp[j] -= 32;
+            j++;
+        }
+        std::cout << tmp << " ";
+        i++;
+    }
+    std::cout << std::endl;
+}
