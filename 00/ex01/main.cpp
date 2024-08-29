e1705d4ab14c01f29a613f40c7a0791383a63cd0
sardamed <mohamedsarda2001@gmail.com>
2024-08-29 19:56:47 +0100

    00/ex01

---
+++ 00/ex01/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+
+int main(int c, char **ar)
+{
+    (void)ar;
+    if (c != 1)
+    {
+        std::cout << "Too many arguments !" << std::endl;
+        return (1);
+    }
+    std::string input;
+    while (1)
+    {
+        std::cout << "-> : ";
+        if (!std::getline(std::cin, input))
+            return (1);
+        std::cout << input << std::endl;
+        if (input == "ADD")
+        {
+            std::cout << "do add shit" << std::endl;
+        }
+        else if (input == "SEARCH")
+            std::cout << "do search shit" << std::endl;
+        else if (input == "EXIT")
+        {
+            std::cout << "By till next time" << std::endl;
+            exit(0);
+        }
+        else
+            continue ;
+    }
+}
