938d6487732d41b1118cb869c326d6bc2f57554e
Mohamed Sarda <msarda@e1r3p9.1337.ma>
2024-09-25 14:21:32 +0100

    01

--- 01/ex06/Harl.cpp
+++ 01/ex06/Harl.cpp
@@ -29,23 +29,42 @@ void Harl::complain( std::string level )
         if (Levels[i] == level)
          break ;
     }
+    // switch (i)
+    // {
+    //     case 0:
+    //         while (i < 4)
+    //             (this->*functions[i++])();
+    //         break ;
+    //     case 1:
+    //         while (i < 4)
+    //             (this->*functions[i++])();
+    //         break ;
+    //     case 2:
+    //         while (i < 4)
+    //             (this->*functions[i++])();
+    //         break ;
+    //     case 3:
+    //         while (i < 4)
+    //             (this->*functions[i++])();
+    //         break ;
+    //     default:
+    //         std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+    //         return ;
+    // }
     switch (i)
     {
         case 0:
-            while (i < 4)
-                (this->*functions[i++])();
-            break ;
+            std::cout << "[ DEBUG ]" << std::endl;
+            (this->*functions[0])();
         case 1:
-            while (i < 4)
-                (this->*functions[i++])();
-            break ;
+            std::cout << "[ INFO ]" << std::endl;
+            (this->*functions[1])();
         case 2:
-            while (i < 4)
-                (this->*functions[i++])();
-            break ;
+            std::cout << "[ WARNING ]" << std::endl;
+            (this->*functions[2])();
         case 3:
-            while (i < 4)
-                (this->*functions[i++])();
+            std::cout << "[ ERROR ]" << std::endl;
+            (this->*functions[3])();
             break ;
         default:
             std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
@@ -56,21 +75,17 @@ void Harl::complain( std::string level )
 
 void Harl::debug( void )
 {
-    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!\n" << std::endl;
 }
 void Harl::info( void )
 {
-    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more\n" << std::endl;
 }
 void Harl::warning( void )
 {
-    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n" << std::endl;
 }
 void Harl::error( void )
 {
-    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
 }
