6e5668969eeec110c02f0ae4b2799c34bcc0e170
sardamed <mohamedsarda2001@gmail.com>
2024-10-01 17:12:34 +0100

    01

---
+++ 02/ex01/Fixed.cpp
@@ -0,0 +1,84 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+// Initialize fractionNum to 256 directly
+const int Fixed::fractionNum = 256;
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called" << std::endl;
+    this->number = 0;
+}
+
+Fixed::Fixed(const Fixed &obj)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = obj;
+}
+
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->number = value * fractionNum; // Multiply by 256
+}
+
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->number = roundf(value * fractionNum); // Multiply by 256
+}
+
+Fixed &Fixed::operator=(Fixed const &obj)
+{
+    if (this != &obj)
+        this->number = obj.number;
+    std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &a)
+{
+    out << a.toFloat();
+    return (out);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->number;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->number = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    // Convert fixed-point back to float by dividing by 256
+    float value;
+
+    value = (this->number * 1.0) / fractionNum;
+    return value;
+}
+
+int Fixed::toInt(void) const
+{
+    // Convert fixed-point back to int by dividing by 256
+    return this->number / fractionNum;
+}
+
+// 0000 0000 0010 1010
+// 0000 0000 0101 0100
+// 0000 0000 1010 1000
+// 0000 0001 0101 0000
+// 0000 0010 1010 0000
+// 0000 0101 0100 0000
+// 0000 1010 1000 0000
+// 0001 0101 0000 0000
---
+++ 02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include "iostream"
+
+class Fixed
+{
+    private:
+        int number;
+        static const int fractionNum;
+    public:
+        int     getRawBits( void ) const;
+        void    setRawBits( int const raw );
+        //
+        float   toFloat( void ) const;
+        int     toInt( void ) const;
+        //
+        Fixed(const Fixed &obj);
+        Fixed(const int value);
+        Fixed(const float value);
+        //
+        Fixed &operator=(Fixed const &obj);
+        //
+        Fixed();
+        ~Fixed();
+};
+std::ostream &operator<<(std::ostream &out, const Fixed &a);
+
+#endif
