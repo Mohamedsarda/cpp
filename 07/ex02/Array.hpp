fed6cf7a1aa14f76495a9a5a2f1f36ac31b57e90
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-11-30 19:11:11 +0100

    Update : 07

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -1,19 +1,21 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <iostream>
+
 template <typename A> class Array
 {
     private:
         unsigned int size;
         A *arr;
-        /* data */
     public:
-        class ErrorSize : public std::exeption {
+        class ErrorSize : public std::exception {
             public:
-                const char *what() const throw {
+                const char *what() const throw() {
                     return "Size should be bigger then 0 and smaller then the size";
                 }
-        }
+        };
         unsigned int getSize() const {
             return (size);
         }
@@ -25,40 +27,37 @@ template <typename A> class Array
         }
 
         Array(unsigned int _size) {
+            size = _size;
             arr = new A[_size];
         }
 
         Array(Array &copy) {
-            arr = new A[0];
+            size = copy.size;
+            arr = NULL;
             *this = copy;
         }
 
         Array  &operator=(Array  &copy) {
             if (this != &copy)
             {
-                delete arr;
-                arr = new A;
+                if (arr != NULL)
+                    delete[] arr;
+                size = copy.size;
+                arr = new A[size];
                 for (size_t i = 0; i < copy.size; i++)
                     arr[i] = copy.arr[i];
-                size = copy.size;
             }
             return *this;
         }
 
-        A &operator[](unsigned int &i) {
-            if (i < 0 || i >= size)
-                throw ErrorSize();
-            return arr[i];
-        }
-
-        const A &operator[](unsigned int &i) const {
+        A &operator[](unsigned int i) {
             if (i < 0 || i >= size)
                 throw ErrorSize();
             return arr[i];
         }
 
         ~Array() {
-            delete arr;
+            delete[] arr;
         }
 
 
