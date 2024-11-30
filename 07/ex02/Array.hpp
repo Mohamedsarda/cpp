0eb1125137c1aab94230c5aa7d13a35095f569f8
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-11-30 18:54:25 +0100

    Update ::

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -8,15 +8,60 @@ template <typename A> class Array
         A *arr;
         /* data */
     public:
-        unsigned int getSize() const;
+        class ErrorSize : public std::exeption {
+            public:
+                const char *what() const throw {
+                    return "Size should be bigger then 0 and smaller then the size";
+                }
+        }
+        unsigned int getSize() const {
+            return (size);
+        }
+
         //
-        Array();
-        Array(unsigned int _size);
-        Array(Array <A> &copy);
-        Array <A> &operator=(Array <A> &copy);
-        A &operator[](unsigned int &i);
-        const A &operator[](unsigned int &i) const;
-        ~Array();
+        Array() {
+            size = 0;
+            arr = new A[0];
+        }
+
+        Array(unsigned int _size) {
+            arr = new A[_size];
+        }
+
+        Array(Array &copy) {
+            arr = new A[0];
+            *this = copy;
+        }
+
+        Array  &operator=(Array  &copy) {
+            if (this != &copy)
+            {
+                delete arr;
+                arr = new A;
+                for (size_t i = 0; i < copy.size; i++)
+                    arr[i] = copy.arr[i];
+                size = copy.size;
+            }
+            return *this;
+        }
+
+        A &operator[](unsigned int &i) {
+            if (i < 0 || i >= size)
+                throw ErrorSize();
+            return arr[i];
+        }
+
+        const A &operator[](unsigned int &i) const {
+            if (i < 0 || i >= size)
+                throw ErrorSize();
+            return arr[i];
+        }
+
+        ~Array() {
+            delete arr;
+        }
+
+
 };
 
 
