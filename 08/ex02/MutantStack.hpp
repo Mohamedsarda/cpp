6007d9e0dddc6716327afb576c1ba08a1d473f2e
Mohamed Sarda <cbamiixsimo@gmail.com>
2024-12-16 13:00:17 +0100

    08

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -1,4 +1,14 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include "iostream"
+
+class MutantStack {
+    public :
+        MutantStack(const MutantStack &copy);
+        MutantStack &operator=(const MutantStack &copy);
+        MutantStack();
+        ~MutantStack();
+};
+
 #endif
